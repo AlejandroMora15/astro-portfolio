f1e6705ec21e10a760892750efa56b321da7cf53
Alejandro Morales Rios <legorios@gmail.com>
2024-03-15 14:16:40 -0500

    Add @midudev/tailwind-animations package

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations'
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -20,5 +22,5 @@ module.exports = {
 			}
 		},
 	},
-	plugins: [],
+	plugins: [animations],
 }
